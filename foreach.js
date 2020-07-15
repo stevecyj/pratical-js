9269929ab6c101884780d4a1c4fdca37760a4a10
Steve <ycsteven.tsao@gmail.com>
2020-07-16 06:06:57 +0800

    first commit

---
+++ foreach.js
@@ -0,0 +1,7 @@
+var num = [1, 2, 3, 4, 5];
+var total = 0;
+num.forEach(function (item, index) {
+  total += item;
+});
+
+console.log(total);
---
+++ module.js
@@ -0,0 +1,16 @@
+// export default {
+//   myName: "小明",
+//   fn: function () {
+//     console.log(this.myName);
+//   },
+// };
+
+// export function fn() {
+//   console.log("這是具名匯出");
+// }
+
+// export const a = 1;
+
+(function () {
+  console.log("我要直接看結果");
+})();
