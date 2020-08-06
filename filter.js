59e85f0cdc04f68c6d7001d835c7f7d6c4acbb45
Steve_Tsao <steve_tsao@neithnet.com>
2020-08-06 09:34:22 +0800

    '$

--- filter.js
+++ filter.js
@@ -1,5 +1,12 @@
 /* global Vue */
 /* eslint-disable no-new */
+Vue.filter('money', function (num) {
+  console.log('filter', num);
+  var parts = num.toString().split('.');
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return '$' + parts.join('.');
+});
+
 new Vue({
   el: '#app',
   data: {
